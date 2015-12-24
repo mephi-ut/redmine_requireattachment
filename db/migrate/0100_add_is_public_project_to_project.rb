025dcb851d8677712344d6b48eabf17f538850ce
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-12-24 20:19:37 +0300

    Forgot to remove db/ after copying from another plugin

--- db/migrate/0100_add_is_public_project_to_project.rb
+++
@@ -1,9 +0,0 @@
-class AddIsPublicProjectToProject < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :is_public_project, :boolean
-  end
-
-  def self.down
-    remove_column :projects, :is_public_project
-  end
-end
